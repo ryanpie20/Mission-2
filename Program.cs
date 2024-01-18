b42f3bafbb7a4394ad7a224900d33e10f1bce758
c-woodman <cwoodma1@byu.edu>
2024-01-17 18:05:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Ryan Whisenant - Section 4 
+using Mission_2; 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        // have user enter number of rolls 
+        System.Console.WriteLine("Please enter the number of rolls");
+        // read the code 
+        int rolls = int.Parse(System.Console.ReadLine());
+
+        Rolls rs;
+        rs = new Rolls();
+
+        rs.RollCount(rolls);
+
+        // create array to hold rs array 
+        int[] sumCounts = rs.sumCounts;
+
+        
+
+        // totalsum of all the rolls 
+        int[] pctRolls = new int[13];
+
+        for (int i = 0; i < sumCounts.Length; i++)
+        {
+            // calculate the percentage 
+            pctRolls[i] = (int)((float)sumCounts[i] * 100 / rolls );
+        }
+
+        // Display the results
+        for (int i = 2; i <= 12; i++)
+        {
+
+            Console.Write($"{i}. "); 
+
+            for (int j = 0; j < pctRolls[i]; j++)
+            {
+                Console.Write("*");
+            }
+            // Move to the next line after printing all asterisks for this number
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Rolls.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    internal class Rolls
+    {
+        // Property to expose sumCounts
+         public int[] sumCounts
+        {
+            get; private set;
+        }
+
+        public Rolls()
+        {
+            sumCounts = new int[13];
+        }
+        public void RollCount(int r)
+        {
+
+            Random rnd = new Random();
+
+            for (int i = r; i > 0; i--)
+            {
+
+                // create two random rolls of each dice
+                int number1 = rnd.Next(1, 7);
+                int number2 = rnd.Next(1, 7);
+
+                int diceSum = (number1 + number2);
+
+                // increment for each count 
+                sumCounts[diceSum]++;
+            }
+        }
+    }
+}
